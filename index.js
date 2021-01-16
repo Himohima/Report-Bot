3517e88265fd8fde9405d49a05e3e4ebac02a16b
HimoHima <useeplaying@gmail.com>
2021-01-16 10:54:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('module-alias/register')
+
+// const Discord = require('discord.js')
+// const client = new Discord.Client()
+const dotenv = require('dotenv');
+dotenv.config();
+
+const path = require('path')
+const Commando = require('discord.js-commando')
+const loadFeatures = require('@root/features/load-features')
+
+const modLogs = require('@features/mod-logs')
+
+const client = new Commando.CommandoClient({
+  owner: '650709300657782805',
+  commandPrefix: '.',
+})
+
+client.on('ready', async () => {
+  console.log('The client is ready!')
+
+  client.registry
+    .registerGroups([
+      ['misc', 'misc commands'],
+      ['moderation', 'moderation commands'],
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'cmds'))
+
+  //modLogs(client)
+})
+
+client.login(process.env.TOKEN)
